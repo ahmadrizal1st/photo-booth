130f9c331ba5d268fd21e26f15b8069e6a8f81c8
Ahmad Rizal <ahmaddrizalul@gmail.com>
2025-09-15 11:38:48 +0700

    feat: initialize Vue Photo Booth project with basic HTML structure and Vite configuration

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import router from "./router";
 import store from "./store";
-import "./registerServiceWorker";
+import './assets/css/app.css';
 
-createApp(App).use(store).use(router).mount("#app");
+createApp(App).use(store).mount("#app");
